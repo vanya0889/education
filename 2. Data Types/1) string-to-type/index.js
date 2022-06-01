d374bd15360c396f9f0b70f57dd8dda3034aa35a
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-01 09:26:46 +0300

    finished string to type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,7 +1,33 @@
-function stringToType(str) {
-  // Напиши свой код здесь
-};
+let per = "100ab";
 
+function stringToType(str) {
+  let a;
+  if (str === "null") {
+    return null;
+  } else if (str === "true") {
+    return true;
+  } else if (str === "false") {
+    return false;
+  } else if (str === "undefined") {
+    return undefined;
+  } else if (str === "") {
+    return str;
+  } else {
+    a = str;
+  }
+
+  let z = Number(a);
+  debugger
+  if (z == a) {
+    return z;
+
+  } else {
+    return str;
+  }
+
+}
+
+console.log(stringToType(per));
 window.stringToType = stringToType;
 
 export default stringToType;
