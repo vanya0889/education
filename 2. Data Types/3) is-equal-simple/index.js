0882b3fda6690274ba2fdf95c015e1f21785cdb4
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-08 13:31:29 +0300

    finished is-equal-simple task

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,5 +1,32 @@
-function isEqual(a, b) {
-  // Напиши свой код здесь
+function isEqual(obj1, obj2) {
+  if (typeof obj1 !== "object" || typeof obj2 !== "object") {
+    return false;
+  }
+  if (obj1 === undefined || obj2 === undefined) {
+    return false;
+  }
+  if (obj1 === null || obj2 === null) {
+    return false;
+  }
+  let obj1Keys = Object.keys(obj1);
+  let obj2Keys = Object.keys(obj2);
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+  for (let i = 0; i < obj1Keys.length; i++) {
+    if (obj2Keys.includes(obj1Keys[i]) === false) {
+      return false;
+    }
+  }
+  for (let i = 0; i < obj1Keys.length; i++) {
+    if (typeof obj1[obj1Keys[i]] === "object") {
+      return isEqual(obj1[obj1Keys[i]], obj2[obj1Keys[i]]);
+    }
+    if (obj1[obj1Keys[i]] !== obj2[obj2Keys[i]]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 window.isEqual = isEqual;
