4c02e9ba381d1b80b441d19ff91e595cbc38af6a
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-08 13:32:25 +0300

    finished full-sum task

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,25 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+  let arr = [...args];
+  let result = 0;
+
+
+  try {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] != "number") {
+        throw new Error("Wrong Argument Type");
+      } else if (arr[i] === undefined) {
+        return 0;
+      } else {
+        result += arr[i];
+      }
+
+    }
+  } catch (e) {
+    console.log(e)
+  }
+
+
+  return result;
 }
 
 window.fullSum = fullSum;
