902a290ffeada2ef30a86e89c44510a9ec092f0d
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-08 13:34:14 +0300

    finished query-object task

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,7 +1,15 @@
-function queryToObject(query) {
-  // Напиши свой код здесь
-};
+let qu = "";
 
+function queryToObject(query) {
+  let gavno = query;
+  let gavno2= gavno.substring(1);
+  console.log(gavno2);
+  let parts = gavno2.split("&");
+  console.log(parts);
+  let fin = {...parts};
+  return fin;
+}
+console.log(queryToObject(qu));
 window.queryToObject = queryToObject;
 
 export default queryToObject;
