8f34ff5b2e0e70d60e0a850da278a4f516a03308
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-08 13:35:15 +0300

    finished async-sum task

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,7 +1,22 @@
+let per1 = 2;
+let per2 = {fgdfgdfg: "a"};
+
+
 function asyncSum(a, b) {
-  // Напиши свой код здесь
+  return new Promise((resolve, reject) => {
+
+    if (typeof a !== "number" && typeof b !== "number") {
+      reject("Ошибка");
+    }
+    setTimeout(() => {
+      resolve("test", a + b)
+    }, 1000)
+
+
+  })
 }
 
+console.log(asyncSum(per1, per2))
 window.asyncSum = asyncSum;
 
 export default asyncSum;
