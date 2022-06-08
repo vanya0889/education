873d32056015987ce262e9b26569f66fe9e9ce34
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-08 13:36:07 +0300

    finished string-reverse task

--- 5. Others/1) string-reverse/index.js
+++ 5. Others/1) string-reverse/index.js
@@ -6,3 +6,20 @@
   'bob'.reverse() === 'bob';
   'abc123'.reverse() === '321cba';
  */
+
+
+
+
+function stringReverse (word) {
+  let newMas = [...word];
+  console.log(newMas);
+  newMas.reverse();
+  console.log(newMas);
+  let newWord=newMas.join("");
+  console.log(newWord);
+  return newWord;
+}
+console.log(stringReverse("t"));
+window.stringReverse = stringReverse;
+
+export default stringReverse;
