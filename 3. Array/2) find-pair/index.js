d0a23e26c1dc734cf2c94b370791d7c44ffa1d48
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-08 13:33:15 +0300

    finished find-pair task

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,6 +1,16 @@
 function findPair(arr) {
-  // Напиши свой код здесь
-};
+  let sorting = arr.sort();
+  console.log(sorting);
+
+
+  for (let i = 1; i < sorting.length; i++) {
+    if (sorting[i-1] === sorting[i]) {
+      return sorting[i];
+
+    }
+  }
+  return null;
+}
 
 window.findPair = findPair;
 
