547ac512820e712f38b6ca5338436f22862c1f90
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-09 10:24:50 +0300

    finished is-equal-advanced task

--- 5. Others/3) check-email/index.js
+++ 5. Others/3) check-email/index.js
@@ -12,9 +12,13 @@
   isValidEmail("e2xa.mple8@gmail.com") === true
  */
 
-function isValidEmail(email) {
-  // Напиши свой код здесь
+function isValidEmail(e) {
+  let re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+  let valid = re.test(e);
+  if(valid===true) {return valid;}
+  else {return false}
 }
+
 
 window.isValidEmail = isValidEmail;
 
