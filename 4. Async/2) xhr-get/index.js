aa4d184697f249eab09b5bf57a90a64ccd612d5e
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-09 10:00:59 +0300

    finished is-equal-advanced task

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,39 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+  return new Promise((resolve, reject) => {
+
+	let web = new XMLHttpRequest();
+	web.open("GET", url, true);
+	web.setRequestHeader("Content-Type", "application/json");
+	web.responseType = "json";
+	web.onload =  function () {
+	  if (web.status > 199 && web.status <300) {
+		resolve(web.response)
+	  } else {
+		reject(web.response)
+	  }
+	}
+
+	web.send();
+
+
+  })
+
+
+  // let web = new XMLHttpRequest();
+  // web.fetch(url, {
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify(data)
+  // })
+  //
+  //   .then(response => response.status)
+  //   .then(response => response.data)
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .catch(error => alert(error.status));
+  // return web;
 }
 
 window.xhrGet = xhrGet;
