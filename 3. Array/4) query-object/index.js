af0ef852d5695657fa7e9af6a05016dfe0ca168d
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-09 13:22:48 +0300

    finished is-equal-advanced task

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,13 +1,55 @@
-let qu = "";
+let qu = "?";
+
+// function queryToObject(str) {
+//   let a;
+//   if (str === "null") {
+//     return null;
+//   } else if (str === "true") {
+//     return true;
+//   } else if (str === "false") {
+//     return false;
+//   } else if (str === "undefined") {
+//     return undefined;
+//   } else if (str === "") {
+//     return {...str};
+//   } else if (str === "queryString") {
+//     let qr = str;
+//     let qr2= qr.substring(1);
+//     console.log(qr2);
+//     let parts = qr2.split("&");
+//     console.log(parts);
+//     let fin = {...parts};
+//     return fin;
+//   } else {
+//     str.split();
+//     a = {...str};
+//   }
+//
+//   let z = Number(a);
+//   debugger
+//   if (z === a) {
+//     return z;
+//
+//   } else {
+//     return str;
+//   }
+//
+// }
+//
+
+
+
+
+
+
 
 function queryToObject(query) {
-  let qr = query;
-  let qr2= qr.substring(1);
-  console.log(qr2);
-  let parts = qr2.split("&");
-  console.log(parts);
-  let fin = {...parts};
-  return fin;
+  query.substring(1);
+  return JSON.parse('{"' + decodeURI(query)
+    .replace("?", '')
+    .replace(/"/g, '\\"')
+    .replace(/&/g, '","')
+    .replace(/=/g,'":"') + '"}')
 }
 console.log(queryToObject(qu));
 window.queryToObject = queryToObject;
