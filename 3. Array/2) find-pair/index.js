79cc1c7574e5f9fb5a13752c490f4772937e6246
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-14 11:51:30 +0300

    finished stack task

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,14 +1,8 @@
 function findPair(arr) {
-  let sorting = arr.sort();
-  console.log(sorting);
+  arr.sort(function(a, b) {
+    return arr[b] - arr[a];
+  });
 
-
-  for (let i = 1; i < sorting.length; i++) {
-    if (sorting[i-1] === sorting[i]) {
-      return sorting[i];
-
-    }
-  }
   return null;
 }
 
