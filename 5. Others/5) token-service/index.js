188a7fe90635af6b595a8c7766159e576563553e
vanya0889 <bondarenko.fortech@gmail.com>
2022-06-15 10:28:12 +0300

    add files

--- 5. Others/5) token-service/index.js
+++ 5. Others/5) token-service/index.js
@@ -8,7 +8,27 @@
  */
 
 class TokenService {
-  // Напиши свой код здесь
+  constructor() {
+	this.observers = [];
+  }
+
+  subscribe(observer) {
+	this.observers.push(observer);
+  }
+
+  setToken(data) {
+    this.observers.forEach((subscriber) => subscriber(data));
+  }
+
+  removeToken(observer) {
+	this.observers.filter(obs => obs !== observer)
+  }
+
+  getToken(observer) {
+	this.observers.forEach(subscriber => subscriber(observer))
+  }
+
+
 }
 
 window.TokenService = TokenService;
